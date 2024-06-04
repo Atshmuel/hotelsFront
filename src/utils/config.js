2032ea79b25b110a02505ea5819034cdd169efc9
Shmuel atar <shmuelatar@gmail.com>
2024-06-04 10:35:59 +0300

    changed server url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 export const PAGE_SIZE = 10;
-// export const SERVER_URL = "https://api-wildhotel.onrender.com/";
-export const SERVER_URL = "http://localhost:3000/";
+export const SERVER_URL = "https://api-wildhotel.onrender.com/";
+// export const SERVER_URL = "http://localhost:3000/";
 
